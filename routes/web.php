c720662c4e8e94425a8b84eee018e00a96829b5a
jvalenciadev <jpvalencia.developer@gmail.com>
2024-07-12 20:05:52 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,87 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', 'HomeController@redirectAdmin')->name('index');
+Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * Admin routes
+ */
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('/', 'Backend\DashboardController@index')->name('admin.dashboard');
+    Route::resource('roles', 'Backend\RolesController', ['names' => 'admin.roles']);
+    Route::resource('users', 'Backend\UsersController', ['names' => 'admin.users']);
+    Route::resource('admins', 'Backend\AdminsController', ['names' => 'admin.admins']);
+    Route::resource('programa', 'Backend\ProgramaController', ['names' => 'admin.programa']);
+    Route::resource('inscripcion', 'Backend\InscripcionController', ['names' => 'admin.inscripcion']);
+    Route::resource('sede', 'Backend\SedeController', ['names' => 'admin.sede']);
+
+
+    // Login Routes
+    Route::get('/login', 'Backend\Auth\LoginController@showLoginForm')->name('admin.login');
+    Route::post('/login/submit', 'Backend\Auth\LoginController@login')->name('admin.login.submit');
+
+    // Logout Routes
+    Route::post('/logout/submit', 'Backend\Auth\LoginController@logout')->name('admin.logout.submit');
+
+    Route::get('/search-rda', 'Backend\AdminsController@searchRda')->name('admin.search.rda');
+    // Forget Password Routes
+    Route::get('/password/reset', 'Backend\Auth\ForgetPasswordController@showLinkRequestForm')->name('admin.password.request');
+    Route::post('/password/reset/submit', 'Backend\Auth\ForgetPasswordController@reset')->name('admin.password.update');
+});
+
+
+Route::group(['prefix' => 'configuracion'], function () {
+    Route::resource('programa', 'Configuracion\ProgramaController', ['names' => 'configuracion.programa']);
+    Route::resource('restriccion', 'Configuracion\RestriccionController', ['names' => 'configuracion.restriccion']);
+
+    Route::post('storeversion', 'Configuracion\ProgramaController@storeVersion')->name('configuracion.programa.storeversion');
+    Route::post('updateversion/{pv_id}', 'Configuracion\ProgramaController@updateVersion')->name('configuracion.programa.updateversion');
+    Route::delete('deleteversion/{pv_id}', 'Configuracion\ProgramaController@destroyVersion')->name('configuracion.programa.deleteversion');
+
+    Route::post('storetipo', 'Configuracion\ProgramaController@storeTipo')->name('configuracion.programa.storetipo');
+    Route::post('storeturno', 'Configuracion\ProgramaController@storeTurno')->name('configuracion.programa.storeturno');
+    Route::post('storeduracion', 'Configuracion\ProgramaController@storeDuracion')->name('configuracion.programa.storeduracion');
+    Route::post('storemodalidad', 'Configuracion\ProgramaController@storeModalidad')->name('configuracion.programa.storemodalidad');
+
+
+    Route::resource('sede', 'Configuracion\SedeController', ['names' => 'configuracion.sede']);
+
+
+
+});
+
+
+Route::group(['prefix' => 'migration'], function () {
+    Route::post('areatrabajo', 'Migration\OtrosController@areaTrabajo')->name('migration.otros.areatrabajo');
+    Route::post('nivel', 'Migration\OtrosController@nivel')->name('migration.otros.nivel');
+    Route::post('categoria', 'Migration\OtrosController@categoria')->name('migration.otros.categoria');
+    Route::post('subsistema', 'Migration\OtrosController@subsistema')->name('migration.otros.subsistema');
+
+    // MIGRACIONES--------------------------------------------------------------------------------------------------------------------------
+    Route::resource('usuarios', 'Migration\UsuariosController', ['names' => 'migration.usuarios']);
+    Route::resource('otros', 'Migration\OtrosController', ['names' => 'migration.otros']);
+    Route::resource('cargo', 'Migration\CargoController', ['names' => 'migration.cargo']);
+    Route::resource('especialidad', 'Migration\EspecialidadController', ['names' => 'migration.especialidad']);
+    Route::resource('departamento', 'Migration\DepartamentoController', ['names' => 'migration.departamento']);
+    Route::resource('distrito', 'Migration\DistritoController', ['names' => 'migration.distrito']);
+    Route::resource('unidadeducativa', 'Migration\UnidadEducativaController', ['names' => 'migration.unidadeducativa']);
+});
